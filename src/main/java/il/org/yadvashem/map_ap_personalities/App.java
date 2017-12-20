2215f264df38eab7e57db6343ffaf973940de761
Kepa J. Rodriguez <kepa@usve112760.serverprofi24.com>
2017-12-20 12:15:33 +0100

    Add skeleton

---
+++ src/main/java/il/org/yadvashem/map_ap_personalities/App.java
@@ -0,0 +1,13 @@
+package il.org.yadvashem.map_ap_personalities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
