b08a72bda188c4d767bea476d17d10c96729d2a8
Kepa J. Rodriguez <kepa@usve112760.serverprofi24.com>
2017-12-21 14:30:20 +0100

    Parsing of strings to convert in access point objects

---
+++ src/main/java/il/org/yadvashem/map_ap_personalities/ParseAccessPointString.java
@@ -0,0 +1,45 @@
+package il.org.yadvashem.map_ap_personalities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ParseAccessPointString {
+
+	public static AccessPoint parseAccessPointString(String apstring) {
+
+		AccessPoint ap = new AccessPoint();
+
+		List<String> years = new ArrayList<String>();
+		List<String> fragments = new ArrayList<String>();
+		
+		
+		String apstring_normalized = apstring.replace(",", " ").replace("-", " ").replace("(", " ").replace(")", " ")
+				.replace("*", " ").replace(".", " ").replace("/", " ");
+		String[] ap_splitted = apstring_normalized.split(" ");
+
+		for (int i=0; i<ap_splitted.length; i++){
+			Pattern pattern_year = Pattern.compile("(\\d{4})");
+			Pattern pattern_fragment = Pattern.compile("([a-zA-Z]+)");
+			Matcher matcher_year = pattern_year.matcher(ap_splitted[i]);
+			Matcher matcher_fragment = pattern_fragment.matcher(ap_splitted[i]);
+			if (matcher_year.find()){
+				years.add(matcher_year.group(1));
+			} else {
+				if (matcher_fragment.find()){
+					fragments.add(matcher_fragment.group(1));
+					System.out.println(matcher_fragment.group(1));
+				}
+			}
+			
+		}
+
+		ap.setAPText(apstring);
+		ap.setDates(years);
+		ap.setPartsOfName(fragments);
+		
+		return ap;
+	}
+
+}
