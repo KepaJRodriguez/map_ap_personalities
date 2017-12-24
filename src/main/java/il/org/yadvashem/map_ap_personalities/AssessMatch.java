7a8d1594b82a2fe99e8de10c70ef3172959b4641
KepaJRodriguez <kepa.rodriguez@gmail.com>
2017-12-24 13:18:36 +0100

    Assessment wether a pair AccessPoint, Person are a candidate to be a match

---
+++ src/main/java/il/org/yadvashem/map_ap_personalities/AssessMatch.java
@@ -0,0 +1,18 @@
+package il.org.yadvashem.map_ap_personalities;
+
+public class AssessMatch {
+
+	public static boolean assessMatch(){
+		
+		boolean isMatch = false;
+		
+		
+		
+		
+		
+		
+		return isMatch;
+	}
+	
+	
+}
