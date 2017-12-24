a6360be9ce8b1e84fc7524ad71ef849509f44f08
KepaJRodriguez <kepa.rodriguez@gmail.com>
2017-12-24 13:56:14 +0100

    Agoritm to assess similarity based on string similarity and dates

--- src/main/java/il/org/yadvashem/map_ap_personalities/AssessMatch.java
+++ src/main/java/il/org/yadvashem/map_ap_personalities/AssessMatch.java
@@ -2,15 +2,24 @@ package il.org.yadvashem.map_ap_personalities;
 
 public class AssessMatch {
 
-	public static boolean assessMatch(){
+	public static boolean assessMatch(AccessPoint ap, Person person){
 		
 		boolean isMatch = false;
 		
+		double stringSimilarity = ComputeStringSimilarity.computeStringSimilarity(ap, person); 
+		String datesAgreement = CompareDates.compareYears(ap, person);
 		
-		
-		
-		
-		
+		if (stringSimilarity >= .95 && !datesAgreement.equals("NEGATIVE")){
+			isMatch = true;
+		} else {
+			if (stringSimilarity >= .90 && (datesAgreement.equals("TOTAL") || datesAgreement.equals("PARTIAL") )){
+				isMatch = true;
+			} else {
+				if (stringSimilarity >= .85 && !datesAgreement.equals("TOTAL")){
+					isMatch = true;
+				}
+			}			
+		}		
 		return isMatch;
 	}
 	
