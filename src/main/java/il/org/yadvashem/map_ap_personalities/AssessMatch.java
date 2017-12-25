016cca7ced98fa36e5940b94281608b626f5295f
KepaJRodriguez <kepa.rodriguez@gmail.com>
2017-12-25 12:45:05 +0100

    Two different functions of assessment

--- src/main/java/il/org/yadvashem/map_ap_personalities/AssessMatch.java
+++ src/main/java/il/org/yadvashem/map_ap_personalities/AssessMatch.java
@@ -2,26 +2,42 @@ package il.org.yadvashem.map_ap_personalities;
 
 public class AssessMatch {
 
-	public static boolean assessMatch(AccessPoint ap, Person person){
-		
+	public static boolean assessMachNameString(AccessPoint ap, Person person){
 		boolean isMatch = false;
-		
-		double stringSimilarity = ComputeStringSimilarity.computeStringSimilarity(ap, person); 
-		String datesAgreement = CompareDates.compareYears(ap, person);
-		
-		if (stringSimilarity >= .95 && !datesAgreement.equals("NEGATIVE")){
+		double stringSimilarity = ComputeStringSimilarity.computeStringSimilarity(ap, person);
+
+		if (stringSimilarity >= .95) {
 			isMatch = true;
-		} else {
-			if (stringSimilarity >= .90 && (datesAgreement.equals("TOTAL") || datesAgreement.equals("PARTIAL") )){
-				isMatch = true;
-			} else {
-				if (stringSimilarity >= .85 && !datesAgreement.equals("TOTAL")){
-					isMatch = true;
-				}
-			}			
-		}		
+		}
+		
 		return isMatch;
 	}
 	
-	
+	public static boolean assessMatchComplete(AccessPoint ap, Person person) {
+
+		boolean isMatch = false;
+		
+		double stringSimilarity = ComputeStringSimilarity.computeStringSimilarity(ap, person);
+		String datesAgreement = CompareDates.compareYears(ap, person);
+		
+		if (stringSimilarity >= .95) {
+			if (!datesAgreement.equals("NEGATIVE")) {
+				isMatch = true;
+			}
+		} else {
+			if (stringSimilarity >= .90) {
+				if (datesAgreement.equals("TOTAL") || datesAgreement.equals("PARTIAL")) {
+					isMatch = true;
+				}
+			} else {
+				if (stringSimilarity >= .85) {
+					if (!datesAgreement.equals("TOTAL")) {
+						isMatch = true;
+					}
+				}
+			}
+		}
+		return isMatch;
+	}
+
 }
